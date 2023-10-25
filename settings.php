a8261c9a7d73fb007c2af833eb4b3a35d92502a0
Your Name <you@example.com>
2023-10-26 03:35:54 +0700

    c

--- settings.php
+++ settings.php
@@ -6,15 +6,15 @@
 // $password = "141292";
 // $dbname = "s103503476_db";
 
-// $servername = "feenix-mariadb.swin.edu.au";
-// $username = "s103503476";
-// $password = "141292";
-// $dbname = "s103503476_db";
-
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "feenix-mariadb.swin.edu.au";
+$username = "s103503476";
+$password = "141292";
 $dbname = "s103503476_db";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "s103503476_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
