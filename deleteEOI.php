75b976004365b4ec88f21fb05b0e99890a04f41e
Your Name <you@example.com>
2023-10-26 19:40:41 +0700

    sdf

--- deleteEOI.php
+++ deleteEOI.php
@@ -1,23 +1,42 @@
 <?php
 include 'settings.php';
 
+// 
+$EOInumber = isset($_POST['EOInumber']) ? $_POST['EOInumber'] : '';
+$EOInumber_GET = isset($_GET['EOInumber']) ? $_GET['EOInumber'] : '';
 
-$EOInumber = isset($_GET['EOInumber']) ? $_GET['EOInumber'] : '';
+if (!empty($EOInumber_GET)) {
 
+    $sql = "SELECT * FROM eoi";
 
+    $result = $conn->query($sql);
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+    foreach ($row as $key => $item):
+        $sql = "DELETE FROM eoi WHERE `eoi`.`EOInumber` = {$item['EOInumber']}";
+        if ($conn->query($sql)) {
+
+        }
+    endforeach;
+
+    echo '<script>';
+    echo 'alert("ลบข้อมูลสำเร็จ");location.assign("manage.php");';
+    echo '</script>';
+
+}
 
 if (!empty($EOInumber)) {
+    foreach ($EOInumber as $key => $item):
+        $sql = "DELETE FROM eoi WHERE `eoi`.`EOInumber` = $item";
+        if ($conn->query($sql)) {
 
-    $sql = "DELETE FROM eoi WHERE `eoi`.`EOInumber` = $EOInumber";
-    if ($conn->query($sql)) {
-        echo '<script>';
-        echo 'alert("ลบข้อมูลสำเร็จ");location.assign("manage.php");';
-        echo '</script>';
-    } else {
-        echo '<script>';
-        echo 'alert("ลบข้อมูลไม่สำเร็จ");history.back(-1);';
-        echo '</script>';
-    }
+        }
+    endforeach;
+
+
+    echo '<script>';
+    echo 'alert("ลบข้อมูลสำเร็จ");location.assign("manage.php");';
+    echo '</script>';
+
 
 } else {
     echo '<script>';
--- manage.php
+++ manage.php
@@ -2,8 +2,8 @@
 include 'settings.php';
 
 
-$searchText = isset($_GET["s"]) ? $_GET["s"] : '';
-$searchState = isset($_GET["state"]) ? $_GET["state"] : 'all';
+$searchText = isset($_POST["s"]) ? $_POST["s"] : '';
+$searchState = isset($_POST["state"]) ? $_POST["state"] : 'all';
 
 if ($searchState == 'all') {
     $sqlS = '';
@@ -18,16 +18,6 @@ $sql = "SELECT * FROM eoi $sqlText $sqlS";
 $result = $conn->query($sql);
 $row = $result->fetch_all(MYSQLI_ASSOC);
 
-// echo json_encode($row);
-// die();
-
-// $result = mysqli_query($conn, $sql); // run query
-
-// $count = mysqli_num_rows($result); //collect the result in count variable.
-// $order = 1; //start count row1
-
-
-// $row = mysqli_fetch_assoc($result)
 ?>
 <!doctype html>
 <html lang="en">
@@ -53,12 +43,12 @@ $row = $result->fetch_all(MYSQLI_ASSOC);
         ?>
         <h1 class="text-center mt-3">Applicant Info</h1>
 
-        <form action="manage.php">
+        <form action="manage.php" method="post" >
             <div class="row">
                 <div class="col-3">
                     <div class="mb-3">
                         <!-- <label for="exampleInputEmail1" class="form-label">Email address</label> -->
-                        <input type="text" class="form-control" name="s" value="<?php echo $searchText ?>"
+                        <input type="text" class="" name="s" value="<?php echo $searchText ?>"
                             placeholder="กรอกรายละเอียด">
 
                     </div>
@@ -66,7 +56,7 @@ $row = $result->fetch_all(MYSQLI_ASSOC);
                 <div class="col-3">
                     <div class="mb-3">
 
-                        <select id="state" name="state" class="form-select">
+                        <select id="state" name="state" class="">
                             <option <?php echo $searchState == 'all' ? "selected" : "" ?> value="all">---- ทั้งหมด ----
                             </option>
                             <option <?php echo $searchState == 'VIC' ? "selected" : "" ?> value="VIC">VIC</option>
@@ -82,137 +72,141 @@ $row = $result->fetch_all(MYSQLI_ASSOC);
                 </div>
                 <div class="col-3">
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-success btn-sm">ค้นหา</button>
+                        <button type="submit" class=" ">ค้นหา</button>
 
                     </div>
                 </div>
+
+                <div class="col-2"></div>
             </div>
 
 
         </form>
-        <table class="table">
-            <thead class="">
-                <tr>
-                    <th>#</th>
-                    <th>Job</th>
-                    <th>Name</th>
-                    <th>address</th>
-                    <th>state</th>
-                    <th>email</th>
-                    <th>phone</th>
-                    <th>skills</th>
-                    <th>Status</th>
-                    <th><a class="btn btn-sm btn-primary" href="apply.php">Ada Apply</a></th>
+        <form action="deleteEOI.php" method="post">
+            <table class="" border="1" style="width: 100%;  border:1px solid black; ">
+                <thead class="">
+                    <tr style=" border:1px solid black; ">
+                        <th>EOI No</th>
+                        <th>Job</th>
+                        <th>Name</th>
+                        <th>address</th>
+                        <th>state</th>
+                        <th>email</th>
+                        <th>phone</th>
+                        <th>skills</th>
+                        <th>Status</th>
+                        <th> <button type="submit" name="" class=" ">ลบที่เลือก</button>  <button type="button" onclick="setDeleteEoi('all')" name="all" class=" ">ลบทั้งหมด</button></th>
 
-
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($row as $key => $item): ?>
-                    <tr>
-                        <td>
-                            <?php echo ++$key ?>
-                        </td>
-                        <td>
-                            <?php echo $item['job_ref'] ?>
-                        </td>
-                        <td>
-                            <?php echo $item['first_name'] . " " . $item['last_name'] ?>
-                        </td>
-                        <td>
-                            <p class="m-0 p-0">address :
-                                <?php echo $item['address'] ?>
-                            </p>
-                            <p class="m-0 p-0">suburb_town :
-                                <?php echo $item['suburb_town'] ?>
-                            </p>
-                            <p class="m-0 p-0">postcode :
-                                <?php echo $item['postcode'] ?>
-                            </p>
-
-                        </td>
-                        <td>
-                            <?php echo $item['state'] ?>
-                        </td>
-                        <td>
-                            <?php echo $item['email'] ?>
-                        </td>
-                        <td>
-                            <?php echo $item['phone'] ?>
-                        </td>
-                        <td>
-                            <p class="m-0 p-0">skills :
-                                <?php echo $item['skills'] ?>
-                            </p>
-                            <p class="m-0 p-0">other_skills :
-                                <?php echo empty($item['other_skills']) ? "-" : $item['other_skills'] ?>
-                            </p>
-                        </td>
-                        <td>
-                            <?php
-                            switch (strtoupper($item['status'])) {
-                                case 'NEW':
-                                    echo '<span class="badge text-bg-info">New</span>';
-                                    break;
-                                case 'CURRENT':
-                                    echo '<span class="badge text-bg-warning">Current</span>';
-                                    break;
-                                case 'FINAL':
-                                    echo '<span class="badge text-bg-success">Final</span>';
-                                    break;
-                            } ?>
-                        </td>
-                        <td>
-                            <a href="jobs.php?EOInumber=<?php echo $item['EOInumber'] ?>"
-                                class="btn btn-sm btn-success">description</a>
-                            <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#m-<?php echo $item['EOInumber'] ?>">
-                                จัดการ
-                            </button>
-                            <button onclick="setDeleteEoi('<?php echo $item['EOInumber'] ?>')" type="button"
-                                class="btn btn-danger btn-sm">Delete</button>
-                        </td>
-                        <div class="modal fade" id="m-<?php echo $item['EOInumber'] ?>" tabindex="-1"
-                            aria-labelledby=" Label" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <form action="updateEOI.php" method="post">
-                                        <input type="hidden" name="EOInumber" value="<?php echo $item['EOInumber'] ?>">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id=" Label"><?php echo $item['job_ref'] ?> : <?php echo $item['first_name'] . " " . $item['last_name'] ?> </h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="mb-3">
-                                                <label for="exampleInputEmail1" class="form-label">อัพเดตสถานะ </label>
-                                                <select name="status" class="form-select"
-                                                    aria-label="Default select example">
-                                                    <!-- <option selected>---- เลือก ----</option> -->
-                                                    <option <?php echo strtoupper($item['status']) == 'NEW' ? "selected" : "" ?> value="NEW">NEW</option>
-                                                    <option <?php echo strtoupper($item['status']) == 'CURRENT' ? "selected" : "" ?> value="CURRENT">CURRENT</option>
-                                                    <option <?php echo strtoupper($item['status']) == 'FINAL' ? "selected" : "" ?> value="FINAL">FINAL</option>
-                                                </select>
-                                            </div>
-
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary btn-sm"
-                                                data-bs-dismiss="modal">ปิด</button>
-                                            <button type="submit" class="btn btn-success btn-sm">บันทึก</button>
-
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
 
                     </tr>
-                <?php endforeach; ?>
+                </thead>
+                <tbody>
+                    <?php foreach ($row as $key => $item): ?>
+                        <tr style=" border:1px solid black; ">
+                            <td>
+                                <?php echo ++$key ?>  <input type="checkbox"  name="EOInumber[]" value="<?php echo $item['EOInumber'] ?>" >
+                            </td>
+                            <td>
+                                <?php echo $item['job_ref'] ?>
+                            </td>
+                            <td>
+                                <?php echo $item['first_name'] . " " . $item['last_name'] ?>
+                            </td>
+                            <td>
+                                <p class="m-0 p-0">address :
+                                    <?php echo $item['address'] ?>
+                                </p>
+                                <p class="m-0 p-0">suburb_town :
+                                    <?php echo $item['suburb_town'] ?>
+                                </p>
+                                <p class="m-0 p-0">postcode :
+                                    <?php echo $item['postcode'] ?>
+                                </p>
 
-            </tbody>
-        </table>
+                            </td>
+                            <td>
+                                <?php echo $item['state'] ?>
+                            </td>
+                            <td>
+                                <?php echo $item['email'] ?>
+                            </td>
+                            <td>
+                                <?php echo $item['phone'] ?>
+                            </td>
+                            <td>
+                                <p class="m-0 p-0">skills :
+                                    <?php echo $item['skills'] ?>
+                                </p>
+                                <p class="m-0 p-0">other_skills :
+                                    <?php echo empty($item['other_skills']) ? "-" : $item['other_skills'] ?>
+                                </p>
+                            </td>
+                            <td>
+                                <?php
+                                switch (strtoupper($item['status'])) {
+                                    case 'NEW':
+                                        echo '<span class="">New</span>';
+                                        break;
+                                    case 'CURRENT':
+                                        echo '<span class="">Current</span>';
+                                        break;
+                                    case 'FINAL':
+                                        echo '<span class="">Final</span>';
+                                        break;
+                                } ?>
+                            </td>
+                            <td>
+                                <a href="jobs.php?EOInumber=<?php echo $item['EOInumber'] ?>" class=" ">description</a>
+                                <button type="button" class=" " data-bs-toggle="modal"
+                                    data-bs-target="#m-<?php echo $item['EOInumber'] ?>">
+                                    จัดการ
+                                </button>
+                                
+                                <!-- <button onclick="setDeleteEoi('<?php echo $item['EOInumber'] ?>')" type="button"
+                                    class="">Delete</button> -->
+                            </td>
+                            <div class="modal fade" id="m-<?php echo $item['EOInumber'] ?>" tabindex="-1"
+                                aria-labelledby=" Label" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <form action="updateEOI.php" method="post">
+                                            <input type="hidden" name="EOInumber" value="<?php echo $item['EOInumber'] ?>">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id=" Label">
+                                                    <?php echo $item['job_ref'] ?> :
+                                                    <?php echo $item['first_name'] . " " . $item['last_name'] ?>
+                                                </h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="mb-3">
+                                                    <label for="exampleInputEmail1" class="form-label">อัพเดตสถานะ </label>
+                                                    <select name="status" class="" aria-label="Default select example">
+                                                        <!-- <option selected>---- เลือก ----</option> -->
+                                                        <option <?php echo strtoupper($item['status']) == 'NEW' ? "selected" : "" ?> value="NEW">NEW</option>
+                                                        <option <?php echo strtoupper($item['status']) == 'CURRENT' ? "selected" : "" ?> value="CURRENT">CURRENT</option>
+                                                        <option <?php echo strtoupper($item['status']) == 'FINAL' ? "selected" : "" ?> value="FINAL">FINAL</option>
+                                                    </select>
+                                                </div>
 
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="" data-bs-dismiss="modal">ปิด</button>
+                                                <button type="submit" class=" ">บันทึก</button>
+
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </form>
 
 
     </div>
